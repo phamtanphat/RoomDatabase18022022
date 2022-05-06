39506d00ded711a6a3a62ae8bec03366f7071f59
Phat <phatdroid94@gmail.com>
2022-05-06 19:25:35 +0700

    entity

---
+++ app/src/main/java/com/example/roomdatabase18022022/data/local/DatabaseManager.java
@@ -0,0 +1,4 @@
+package com.example.roomdatabase18022022.data.local;
+
+public class DatabaseManager {
+}
---
+++ app/src/main/java/com/example/roomdatabase18022022/data/local/entities/NoteWord.java
@@ -0,0 +1,47 @@
+package com.example.roomdatabase18022022.data.local.entities;
+
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class NoteWord {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String title;
+    private String description;
+
+    public NoteWord() {
+    }
+
+    @Ignore
+    public NoteWord(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
